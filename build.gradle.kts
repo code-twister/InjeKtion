43ff1f39af40e738562aeb3401d23bc4ff120f49
Mihaly Nagy <mihaly.f.nagy@gmail.com>
2025-10-06 19:38:15 +0300

    adding publishing library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.10"
-    id("maven-publish")
-    signing
+    id ("org.danilopianini.publish-on-central") version "9.1.0"
 }
 
 group = "dev.codetwister"
@@ -22,26 +21,21 @@ kotlin {
     jvmToolchain(21)
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
+publishOnCentral {
+    repoOwner.set("code-twister") // Used to populate the default value for projectUrl and scmConnection
+    projectDescription.set("A minimalistic dependency injection library for Kotlin.")
+    // The following values are the default, if they are ok with you, just omit them
+    projectLongName.set("injeKtion")
+    licenseName.set("Gnu General Public License v3.0")
+    licenseUrl.set("https://www.gnu.org/licenses/gpl-3.0.en.html")
+    projectUrl.set("https://github.com/${repoOwner.get()}/InjeKtion")
+    scmConnection.set("scm:git:https://github.com/${repoOwner.get()}/InjeKtion")
 }
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-
+        withType<MavenPublication> {
             pom {
-                name = "injeKtion"
-                description = "A minimalistic dependency injection library for Kotlin."
-                url = "https://github.com/code-twister/InjeKtion"
-                licenses {
-                    license {
-                        name = "GNU General Public License v3.0"
-                        url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
-                    }
-                }
                 developers {
                     developer {
                         name = "Nagy Mihaly Ferencz"
@@ -49,11 +43,6 @@ publishing {
                         organization = "codetwister"
                         organizationUrl = "https://codetwister.dev"
                     }
-                }
-                scm {
-                    connection = "scm:git:git://github.com/code-twister/InjeKtion.git"
-                    developerConnection = "scm:git:ssh://github.com:code-twister/InjeKtion.git"
-                    url = "https://github.com/code-twister/InjeKtion"
                 }
             }
         }
@@ -65,5 +54,4 @@ signing {
     val signingKey = project.findProperty("signingKey").toString()
     val signingPassword = project.findProperty("signingPassword").toString()
     useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-    sign(publishing.publications["maven"])
 }
